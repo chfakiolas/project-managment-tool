cc33073df6ef71b2b941cc78042a19364515fa02
Chris Fakiolas <fakiolas.ch@gmail.com>
2025-10-18 22:26:38 +0300

    feat: added rest framework and cors headers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -127,3 +127,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INSTALLED_APPS += [
+    'rest_framework',
+    'corsheaders',
+]
+
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')
+
+CORS_ALLOW_ALL_ORIGINS = True
